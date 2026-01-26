cecf1635221b842bd3f0ecb5348f388a271027e5
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-01-26 11:16:54 +0700

    Update srk.py

--- srk.py
+++ srk.py
@@ -57,9 +57,9 @@ def display_header():
       
     # Versi dan URL
     print(f"{Fore.BLUE}╔{'═' * 68}╗")
-    print(f"{Fore.BLUE}║\033[48;5;4m{' ' * 22}\033[37mA u t h o r :  K u n F z{' ' * 21} {Fore.BLUE}\033[0m║")
-    print(f"{Fore.BLUE}║\033[48;5;4m{' ' * 19}\033[37mA d m :  Z' b l a c k  3 1 3{' ' * 20} {Fore.BLUE}\033[0m║")
-    print(f"{Fore.BLUE}║\033[48;5;4m{' ' * 10}\033[37mC a n  b e  u s e d  b u t  n o t  s h a r e d{' ' * 11} {Fore.BLUE}\033[0m║")
+    print(f"{Fore.BLUE}║\033[48;5;4m{' ' * 22}\033[37mA u t h o r :  K u n F z{' ' * 21} {Fore.BLUE}║\033[0m")
+    print(f"{Fore.BLUE}║\033[48;5;4m{' ' * 19}\033[37mA d m :  Z' b l a c k  3 1 3{' ' * 20} {Fore.BLUE}║\033[0m")
+    print(f"{Fore.BLUE}║\033[48;5;4m{' ' * 10}\033[37mC a n  b e  u s e d  b u t  n o t  s h a r e d{' ' * 11} {Fore.BLUE}║\033[0m")
     print(f"{Fore.BLUE}╚{'═' * 68}╝")
 
 # Fungsi untuk Meminta Input dari Pengguna dengan Tampilan Rapi
